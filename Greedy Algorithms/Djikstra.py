6cf0ab072df6a3867f2f8b3288fc3623ee10eaaa
Pedro Felippe <pedfelippe@gmail.com>
2020-10-12 04:13:09 -0300

    refactor/djikstra

--- Greedy Algorithms/Djikstra.py
+++ Greedy Algorithms/Djikstra.py
@@ -4,44 +4,42 @@ sys.path.insert(0, './lib')
 
 from Graph import *
 
-import heapq
-
 from collections import OrderedDict
-
+#
 def createHeap(G): # nlogn (i guess)
     sorted_x = sorted(G.vert_dict.items(), key = lambda x:
-                                                G.get_vertice_by_id(x[0]).distance) # sort the "vert_dict" by vertex distance
+                                                G.getVertexById(x[0]).getDistance()) # sort the "vert_dict" by vertex distance
     
     return OrderedDict(sorted_x)
 
 def extractMin(G, D): 
     vertex = D.popitem(last=False)
-    u = G.get_vertice_by_id(vertex[0])
+    u = G.getVertexById(vertex[0])
     return u, D
     
-def relaxEdge(u, v):
-    distanceU = u.get_distance()
-    distanceV = v.get_distance()
-    w = u.get_weight(v)
+def relaxEdge(G, edge):
+    
+    u,v,w = edge
+    
+    u = G.getVertexById(u.getId())
+    v = G.getVertexById(v.getId())
+    
+    distanceU = u.getDistance()
+    distanceV = v.getDistance()
     
     if (distanceU + w < distanceV):
-        v.set_distance(int(distanceU) + int(w))
-        v.set_previous(u)
-
-    
-def mantemHeap(S):
-    print('actualize heap')
+        v.setDistance(distanceU + w)
 
 def printSolution(S):
-    print("DJIKSTRA ALGORITHM")
+
     for i in S:
         print('----------------------------------------------')
-        print(f' Vertex: |{i.id}| - Distance from source: |{i.distance}|') 
-    
+        print(f' Vertex: |{i.getId()}| - Distance from source: |{i.getDistance()}|') 
     print('----------------------------------------------')
     
 def dijkstra(G, s):
-    s.distance = 0
+    print("Djikstra's shortest path")
+    s.setDistance(0)
     S = {}
     S[s] = s
     Q = createHeap(G) 
@@ -49,8 +47,8 @@ def dijkstra(G, s):
     while (len(Q) > 1):
         u, Q = extractMin(G, Q)
         S[u] = u
-        for neighbour in u.get_neighbors():
-            relaxEdge(u, neighbour)
+        for edge in u.getEdges():
+            relaxEdge(G, edge)
         
     printSolution(S)
 
@@ -58,21 +56,21 @@ if __name__ == '__main__':
 
     g = Graph()
 
-    a = g.add_vertex('a')
-    b = g.add_vertex('b')
-    c = g.add_vertex('c')
-    d = g.add_vertex('d')
-    e = g.add_vertex('e')
-    f = g.add_vertex('f')
+    a = g.addVertex('a')
+    b = g.addVertex('b')
+    c = g.addVertex('c')
+    d = g.addVertex('d')
+    e = g.addVertex('e')
+    f = g.addVertex('f')
 
-    g.add_edge(a, b, 7)  
-    g.add_edge(a, b, 6)
-    g.add_edge(a, f, 14)
-    g.add_edge(b, c, 10)
-    g.add_edge(b, d, 15)
-    g.add_edge(c, d, 11)
-    g.add_edge(c, f, 2)
-    g.add_edge(d, e, 6)
-    g.add_edge(e, f, 9)
+    g.addEdge(a, b, 6) 
+    g.addEdge(a, b, 7)  
+    g.addEdge(a, f, 14)
+    g.addEdge(b, c, 10)
+    g.addEdge(b, d, 15)
+    g.addEdge(c, d, 11)
+    g.addEdge(c, f, 2)
+    g.addEdge(d, e, 6)
+    g.addEdge(e, f, 9)
 
     dijkstra(g, a)
--- lib/Graph.py
+++ lib/Graph.py
@@ -3,61 +3,37 @@ import sys
 class Vertex:
     def __init__(self, node):
         self.id = node
-        self.adjacent = {}
+        self.edges = []
         self.distance = sys.maxsize    
-        self.visited = False  
-        self.previous = None
 
-    def add_neighbor(self, neighbor, weight):
-        self.adjacent[neighbor] = weight
-    
-    def get_id(self):
+    def getId(self):
         return self.id
 
-    def get_neighbors(self):
-        return self.adjacent
-
-    def get_weight(self, neighbor):
-        return self.adjacent[neighbor]
-
-    def get_distance(self):
+    def getDistance(self):
         return self.distance
 
-    def get_previous(self):
-        return self.previous
-
-    def set_distance(self, dist):
+    def setDistance(self, dist):
         self.distance = dist
-
-    def set_previous(self, prev):
-        self.previous = prev
-
-    def set_visited(self):
-        self.visited = True
     
-    def get_weight(self, neighbor):
-        return self.adjacent[neighbor]
-
+    def getEdges(self):
+        return self.edges
+    
 class Graph():
     def __init__(self):
         self.vert_dict = {}
         self.num_vertices = 0
 
-    def add_vertex(self, v):
-        self.num_vertices = self.num_vertices + 1
+    def addVertex(self, v):
         vertex = Vertex(v)
         self.vert_dict[v] = vertex
         return vertex
 
-    def add_edge(self, i, f, w=0):
-        i.add_neighbor(f, w)
+    def addEdge(self, i, f, w=0):
+        i.edges.append([i, f, w])
 
-    def get_vertice_by_id(self, id):
+    def getVertexById(self, id):
         return self.vert_dict[id]
 
     def printSolution(self): 
         for v in range(self.vert_dict): 
-            print ("vertex: " + v + " - distance: " + v.distance) 
-
-    def get_dict(self):
-        return self.vert_dict
+            print ("vertex: " + v + " - distance: " + v.getDistance()) 
